7e85c97cbf96d9eb1a88768ef176d1cdd4bbd392
Eliane <lidejesusol@yahoo.com.br>
2023-10-24 21:40:36 -0300

    commit da aula 24.10

---
+++ main.dart
@@ -0,0 +1,35 @@
+void main () {
+//   String nome = 'Eliane';
+  
+//   int idade = 49;
+
+// if (idade >=18) {
+//   print('$nome é maior de idade');
+// } else if (idade< 18 && idade > 12) {
+//   print('$nome é adolescente');
+  
+// } else {
+//   print ('$nome é criança');
+// }
+
+
+
+int idade = 49;
+
+if (idade >0 && idade <=12) {
+  print ('$nome é criança');
+
+  } else if (idade >= 13 && idade<= 17) {
+print ('$nome é adolescente');
+
+} else if (idade >= 18 && idade <= 60) {
+  print ('$nome é adulto');
+
+  } else {
+    print ('nome é idoso');
+  }
+}
+  }
+}
+
+}
