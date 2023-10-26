41211137834fa9fc92f2daf5963b7ca5af568582
Eliane <lidejesusol@yahoo.com.br>
2023-10-25 21:52:25 -0300

    commit da aula 25.10

---
+++ Aula 25.10/main.dart
@@ -0,0 +1,22 @@
+void main() {
+
+  double temperatura = -30; 
+
+if (temperatura >= -30 && temperatura <= 0) {
+  print (é muito frio');
+
+
+} else if (temperatura > 0 && temperatura < 12) {
+  print ('frio');
+
+  else if (temperatura > 12 && temperatura < 20);
+  print (temperatura);
+} 
+
+else if (temperatura >20 && temperatura < 31);
+print (temperatura):
+
+else if (temperatura > 31);
+print (temperatura);
+
+}
