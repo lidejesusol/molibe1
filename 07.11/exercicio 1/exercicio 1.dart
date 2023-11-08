04c64ba29455ea44a9fc7a55e303cc225c3d39ff
Aluno Senai <lidejesusol@yahoo.com.br>
2023-11-07 21:46:36 -0300

    commit da aula 07.11

---
+++ 07.11/exercicio 1/exercicio 1.dart
@@ -0,0 +1,12 @@
+ int soma(int a, int b){
+  return a + b;
+
+ }
+ void main(){
+  int numero1 = 5;
+  int numero2 = 3;
+  int resultado = soma(numero1, numero2);
+  print('A soma de $numero1 e $numero2 é $resultado.');
+  
+ }
+
